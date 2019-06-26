bb0d2c603cfe8830b9f3630be873928109ce93a7
Daniel Voigt <danielhvoigt@gmail.com>
2019-06-26 05:49:52 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,24 @@
+class Song
+
+    @@all = {}
+    @@count = 0
+ 
+  attr_accessor :name, :artist, :genre
+ 
+  def initialize(name, artist, genre)
+    @name = name
+    @artist = artist
+    @genre = genre
+    @@count += 1
+    @@all << self
+  end
+ 
+  def self.all
+    @@all
+  end
+  
+  def self.count
+    return @@count
+  end
+
+end
